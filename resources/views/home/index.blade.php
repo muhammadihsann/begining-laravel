1b223bb1992cf2d5189a28cb77b8b39c8761a5f3
BogelDR <muhammad-ihsan@outlook.com>
2015-10-09 11:44:39 +0700

    adding header

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.header')
+
+
+
+@section('content')
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam beatae numquam a, nobis nesciunt quia laudantium dolor sit expedita libero ullam iusto qui neque, odit unde repellat esse magnam dolorem!</p>
+
+@stop
